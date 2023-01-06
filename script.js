4a6c449356eebf4e7405c817c5c04ba0d28e1692
devcristi <121441678+devcristi@users.noreply.github.com>
2023-01-06 14:38:01 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -57,44 +57,6 @@ const homeElements = document.querySelectorAll('.home');
 homeElements.forEach((el) => observer.observe(el));
 
 
-/* -- Carousel Navigation -- */
-
-let activeIndex = 0;
-
-const slides = document.getElementsByTagName("article");
-
-const handleLeftClick = () => {
-  const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : slides.length - 1;
-  
-  const currentSlide = document.querySelector(`[data-index="${activeIndex}"]`),
-        nextSlide = document.querySelector(`[data-index="${nextIndex}"]`);
-        
-  currentSlide.dataset.status = "after";
-  
-  nextSlide.dataset.status = "becoming-active-from-before";
-  
-  setTimeout(() => {
-    nextSlide.dataset.status = "active";
-    activeIndex = nextIndex;
-  });
-}
-
-const handleRightClick = () => {
-  const nextIndex = activeIndex + 1 <= slides.length - 1 ? activeIndex + 1 : 0;
-  
-  const currentSlide = document.querySelector(`[data-index="${activeIndex}"]`),
-        nextSlide = document.querySelector(`[data-index="${nextIndex}"]`);
-  
-  currentSlide.dataset.status = "before";
-  
-  nextSlide.dataset.status = "becoming-active-from-after";
-  
-  setTimeout(() => {
-    nextSlide.dataset.status = "active";
-    activeIndex = nextIndex;
-  });
-}
-
 /* -- Mobile Nav Toggle -- */
 
 const nav = document.querySelector("nav");
